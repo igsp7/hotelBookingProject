6535b0c332fa3668e688e0022770ba19f2d77840
Dimitris Sourailidis <37817259+dimsour@users.noreply.github.com>
2019-01-13 23:31:02 +0200

    dimsour first commit

--- ourRooms.php
+++ ourRooms.php
@@ -1,3 +1,8 @@
+<?php
+  include_once("config.php");
+  $result = mysqli_query($mysqli, "SELECT * FROM rooms");
+
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -50,12 +55,29 @@
   </div>
 
 </nav>
-<div class='card flex-sm-row mb-gutter'><img class="card-img-top card-img-sm-left" src="room1.jpg"/>
-  <div class="card-body">
-    <h4 class="card-title">Basic Room</h4>
-    <p class="card-text">1 Bed <br> 1 Bathroom <br> TV </p>
-  </div>
-</div>
+
+<?php
+while ($res=mysqli_fetch_array($result))
+{
+  $room_number = $res['room_number'];
+  $room_floor= $res['floor'];
+  $room_price= $res['price'];
+  $room_type=$res['room_type'];
+  $beds_number=$res['beds_number'];
+  $bathrooms_number=$res['bathrooms_number'];
+  $room_description = $res['room_description'];
+  $photo =$res['photo'];
+
+
+  echo "<div class='card flex-sm-row mb-gutter'><img id=roomPhoto class='card-img-top card-img-sm-left' src='$photo'/>
+    <div class='card-body'>
+      <h4 class='card-title'>$room_type Room</h4>
+      <p class='card-text'>Room Number: $room_number<br> $room_description <br> Price : $room_price$ <br> Room Number : $room_number <br> Room Floor : $room_floor <br> Number of Beds : $beds_number <br> Number of Bathrooms : $bathrooms_number</p>
+    </div>
+  </div>";
+}
+?>
+
 </div>
   </body>
 </html>
