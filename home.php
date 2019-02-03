7fbf5c72deb29059eeceb5b19f0d44204fe0dbd8
IgorSpi <igor13pao@hotmail.com>
2019-02-03 05:06:23 +0200

    remove unnecessary files
    Co-Authored-By: Dimitris Sourailidis <dimsour@users.noreply.github.com>

--- home.php
+++ home.php
@@ -11,8 +11,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="css/postboot.min.css"/>
     <link rel="stylesheet" href="css/style.css"/>
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
--- users.php
+++ users.php
@@ -16,8 +16,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="css/postboot.min.css"/>
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"/> -->
+    <!-- <link rel="stylesheet" href="https://cdn.rawgit.com/tarkhov/postboot/v1.0.0-beta3/dist/css/postboot.min.css""/> -->
     <link rel="stylesheet" href="css/style.css"/>
     <link rel="stylesheet" type="text/css" href="style.css">
 
